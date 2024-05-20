adccdc177b8c7be493ed5300e2d0f364cb17d2a1
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2024-05-20 15:44:20 -0400

    WIP analytics implementation: * changes migration 0024:  - creates Event model  - in Session model makes ip_address and user_agent optional  - removes session_id from Session model (no link with django session)  - removes modification of the Activity model * adds migration 0027 to populate analytics.Event with some data from Activity - may be incomplete * modifies analytics.py - PEP8, comments out Dimension and Metric models

--- askbot/models/analytics.py
+++ askbot/models/analytics.py
@@ -1,50 +1,50 @@
+"""Models for the Analytics feature"""
 from django.db import models
 from django.db.models import Q
 from django.db.models import Value
 from django.db.models.functions import Substr, StrIndex
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.conf import settings as django_settings
 from django.utils.translation import gettext_lazy as _
 
-"""
-for convenience, here are the activity types from used in the Activity object
-TYPE_ACTIVITY_ASK_QUESTION = 1
-TYPE_ACTIVITY_ANSWER = 2
-TYPE_ACTIVITY_COMMENT_QUESTION = 3
-TYPE_ACTIVITY_COMMENT_ANSWER = 4
-TYPE_ACTIVITY_UPDATE_QUESTION = 5
-TYPE_ACTIVITY_UPDATE_ANSWER = 6
-TYPE_ACTIVITY_PRIZE = 7
-TYPE_ACTIVITY_MARK_ANSWER = 8
-TYPE_ACTIVITY_VOTE_UP = 9
-TYPE_ACTIVITY_VOTE_DOWN = 10
-TYPE_ACTIVITY_CANCEL_VOTE = 11
-TYPE_ACTIVITY_DELETE_QUESTION = 12
-TYPE_ACTIVITY_DELETE_ANSWER = 13
-TYPE_ACTIVITY_MARK_OFFENSIVE = 14
-TYPE_ACTIVITY_UPDATE_TAGS = 15
-TYPE_ACTIVITY_FAVORITE = 16
-TYPE_ACTIVITY_USER_FULL_UPDATED = 17
-TYPE_ACTIVITY_EMAIL_UPDATE_SENT = 18
-TYPE_ACTIVITY_MENTION = 19
-TYPE_ACTIVITY_UNANSWERED_REMINDER_SENT = 20
-TYPE_ACTIVITY_ACCEPT_ANSWER_REMINDER_SENT = 21
-TYPE_ACTIVITY_CREATE_TAG_WIKI = 22
-TYPE_ACTIVITY_UPDATE_TAG_WIKI = 23
-TYPE_ACTIVITY_MODERATED_NEW_POST = 24
-TYPE_ACTIVITY_MODERATED_POST_EDIT = 25
-TYPE_ACTIVITY_CREATE_REJECT_REASON = 26
-TYPE_ACTIVITY_UPDATE_REJECT_REASON = 27
-TYPE_ACTIVITY_VALIDATION_EMAIL_SENT = 28
-TYPE_ACTIVITY_POST_SHARED = 29
-TYPE_ACTIVITY_ASK_TO_JOIN_GROUP = 30
-TYPE_ACTIVITY_MODERATION_ALERT_SENT = 31
-TYPE_ACTIVITY_FORBIDDEN_PHRASE_FOUND = 50 #added gap
-TYPE_ACTIVITY_USER_REGISTERED = 51
-TYPE_ACTIVITY_QUESTION_VIEWED = 52
-TYPE_ACTIVITY_ANSWER_VIEWED = 53
-""" #pylint: disable=pointless-string-statement
+#for convenience, here are the activity types from used in the Activity object
+#TYPE_ACTIVITY_ASK_QUESTION = 1
+#TYPE_ACTIVITY_ANSWER = 2
+#TYPE_ACTIVITY_COMMENT_QUESTION = 3
+#TYPE_ACTIVITY_COMMENT_ANSWER = 4
+#TYPE_ACTIVITY_UPDATE_QUESTION = 5
+#TYPE_ACTIVITY_UPDATE_ANSWER = 6
+#TYPE_ACTIVITY_PRIZE = 7
+#TYPE_ACTIVITY_MARK_ANSWER = 8
+#TYPE_ACTIVITY_VOTE_UP = 9
+#TYPE_ACTIVITY_VOTE_DOWN = 10
+#TYPE_ACTIVITY_CANCEL_VOTE = 11
+#TYPE_ACTIVITY_DELETE_QUESTION = 12
+#TYPE_ACTIVITY_DELETE_ANSWER = 13
+#TYPE_ACTIVITY_MARK_OFFENSIVE = 14
+#TYPE_ACTIVITY_UPDATE_TAGS = 15
+#TYPE_ACTIVITY_FAVORITE = 16
+#TYPE_ACTIVITY_USER_FULL_UPDATED = 17
+#TYPE_ACTIVITY_EMAIL_UPDATE_SENT = 18
+#TYPE_ACTIVITY_MENTION = 19
+#TYPE_ACTIVITY_UNANSWERED_REMINDER_SENT = 20
+#TYPE_ACTIVITY_ACCEPT_ANSWER_REMINDER_SENT = 21
+#TYPE_ACTIVITY_CREATE_TAG_WIKI = 22
+#TYPE_ACTIVITY_UPDATE_TAG_WIKI = 23
+#TYPE_ACTIVITY_MODERATED_NEW_POST = 24
+#TYPE_ACTIVITY_MODERATED_POST_EDIT = 25
+#TYPE_ACTIVITY_CREATE_REJECT_REASON = 26
+#TYPE_ACTIVITY_UPDATE_REJECT_REASON = 27
+#TYPE_ACTIVITY_VALIDATION_EMAIL_SENT = 28
+#TYPE_ACTIVITY_POST_SHARED = 29
+#TYPE_ACTIVITY_ASK_TO_JOIN_GROUP = 30
+#TYPE_ACTIVITY_MODERATION_ALERT_SENT = 31
+#TYPE_ACTIVITY_FORBIDDEN_PHRASE_FOUND = 50 #added gap
+#TYPE_ACTIVITY_USER_REGISTERED = 51
+#TYPE_ACTIVITY_QUESTION_VIEWED = 52
+#TYPE_ACTIVITY_ANSWER_VIEWED = 53
 
 EVENT_TYPES = (
     (1, _('registered')), # Activity.activity_type == 51
@@ -63,21 +63,21 @@ EVENT_TYPES = (
     (14, _('searched')),
 )
 
-"""
-class Dimension(models.Model):
-    name = models.CharField(max_length=64, help_text="Name of the dimension")
-    description = models.TextField(blank=True, null=True, help_text="Description of the dimension")
-    query = models.CharField(max_length=256, help_text="Django ORM query, Python code string")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Metric(models.Model):
-    name = models.CharField(max_length=64, help_text="Name of the metric")
-    description = models.TextField(blank=True, null=True, help_text="Description of the metric")
-    query = models.CharField(max_length=256, help_text="Django ORM query, Python code string")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-"""
+# Dimension and Metric would make a generic implementation of the analytics feature
+# however it is more complex.
+#class Dimension(models.Model):
+#    name = models.CharField(max_length=64, help_text="Name of the dimension")
+#    description = models.TextField(blank=True, null=True, help_text="Description of the dimension")
+#    query = models.CharField(max_length=256, help_text="Django ORM query, Python code string")
+#    created_at = models.DateTimeField(auto_now_add=True)
+#    updated_at = models.DateTimeField(auto_now=True)
+#
+#class Metric(models.Model):
+#    name = models.CharField(max_length=64, help_text="Name of the metric")
+#    description = models.TextField(blank=True, null=True, help_text="Description of the metric")
+#    query = models.CharField(max_length=256, help_text="Django ORM query, Python code string")
+#    created_at = models.DateTimeField(auto_now_add=True)
+#    updated_at = models.DateTimeField(auto_now=True)
 
 def get_non_admins_count():
     """Returns the count of non-admin users, as relevant for Askbot analytics"""
@@ -110,20 +110,23 @@ class Session(models.Model):
     ip_address = models.GenericIPAddressField(null=True, blank=True)
     user_agent = models.CharField(max_length=512, null=True, blank=True)
     created_at = models.DateTimeField() # no auto_now_ad or auto_now for created_at and updated_at
-    updated_at = models.DateTimeField() # because we want to set it manually for the testing purposes
+    updated_at = models.DateTimeField() # b/c we want to set it manually for the testing purposes
 
     def __str__(self):
-        return f"Session: {self.user.email} {self.created_at.isoformat()} - {self.updated_at.isoformat()}"
+        created_at = self.created_at.isoformat() # pylint: disable=no-member
+        updated_at = self.updated_at.isoformat() # pylint: disable=no-member
+        email = self.user.email # pylint: disable=no-member
+        return f"Session: {email} {created_at} - {updated_at}"
 
 class Event(models.Model):
     """Analytics event"""
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
     event_type = models.CharField(max_length=64, choices=EVENT_TYPES)
-    created_at = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField(db_index=True)
-    content_object = fields.GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey('content_type', 'object_id')
 
     def __str__(self):
-        return f"Event: {self.event_type} {self.created_at.isoformat()}"
-
+        timestamp = self.timestamp.isoformat() # pylint: disable=no-member
+        return f"Event: {self.event_type_display} {timestamp}"
